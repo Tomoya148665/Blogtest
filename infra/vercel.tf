46d55b1334578a378306e2f33aa5857e5f4b60d3
kokiabeven <cbucmlgr1@gmail.com>
2023-01-28 04:42:51 +0900

    add infra settings

---
+++ infra/vercel.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+    }
+  }
+}
+
+variable "vercel_api_token" {}
+variable "api_url" {}
+
+provider "vercel" {
+  api_token = var.vercel_api_token
+}
+
+resource "vercel_project" "gorlem" {
+  name            = "jetgee"
+  framework       = "astro"
+  build_command   = "yarn build"
+  team_id         = "team_cUUJOF5e58em9YTg9UnbzH1n" 
+  install_command = "yarn"
+  git_repository = {
+    type = "github"
+    repo = "wrexiv/jetgee"
+  }
+  root_directory = "frontend"
+  environment = [
+    {
+      key    = "API_URL"
+      value  = var.api_url
+      target = ["production", "preview"]
+    },
+  ]
+}
