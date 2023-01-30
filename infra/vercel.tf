2d027e31274a27492f6fb3b4c682ec4109160550
kokiabeven <cbucmlgr1@gmail.com>
2023-01-31 01:11:45 +0900

    infra

--- infra/vercel.tf
+++ infra/vercel.tf
@@ -8,21 +8,23 @@ terraform {
 
 variable "vercel_api_token" {}
 variable "api_url" {}
+variable "storyblok_access_token" {}
 
 provider "vercel" {
   api_token = var.vercel_api_token
 }
 
 resource "vercel_project" "gorlem" {
-  name            = "jetgee"
+  name            = "jetgee-media"
   framework       = "astro"
   build_command   = "yarn build"
   team_id         = "team_cUUJOF5e58em9YTg9UnbzH1n" 
   install_command = "yarn"
   git_repository = {
     type = "github"
-    repo = "wrexiv/jetgee"
+    repo = "wrexiv/jetgee-media"
     branch = "main"
+    production_branch = "main"
   }
   environment = [
     {
@@ -30,5 +32,10 @@ resource "vercel_project" "gorlem" {
       value  = var.api_url
       target = ["production", "preview"]
     },
+    {
+      key = "VITE_STORYBLOK_ACCESS_TOKEN"
+      value = var.storyblok_access_token
+      target = ["production", "preview"]
+    }
   ]
 }
