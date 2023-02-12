00e7c2682cd705a355aee51bd8f620aae29e81b4
kokiabeven <cbucmlgr1@gmail.com>
2023-02-12 18:03:23 +0900

    add env

--- infra/vercel.tf
+++ infra/vercel.tf
@@ -8,13 +8,18 @@ terraform {
 
 variable "vercel_api_token" {}
 variable "api_url" {}
-variable "storyblok_access_token" {}
+variable "contentful_delivery_access_token" {}
+variable "contentful_space_id" {}
+variable "contentful_environment" {}
+variable "contentful_management_token" {
+  sensitive = true
+}
 
 provider "vercel" {
   api_token = var.vercel_api_token
 }
 
-resource "vercel_project" "gorlem" {
+resource "vercel_project" "jetgee-media" {
   name            = "jetgee-media"
   framework       = "astro"
   build_command   = "yarn build"
@@ -34,8 +39,23 @@ resource "vercel_project" "gorlem" {
       target = ["production", "preview"]
     },
     {
-      key = "VITE_STORYBLOK_ACCESS_TOKEN"
-      value = var.storyblok_access_token
+      key = "VITE_CONTENTFUL_DELIVERY_ACCESS_TOKEN"
+      value = var.contentful_delivery_access_token 
+      target = ["production", "preview"]
+    },
+    {
+      key = "VITE_CONTENTFUL_SPACE_ID"
+      value = var.contentful_space_id
+      target = ["production", "preview"]
+    },
+    {
+      key = "VITE_CONTENTFUL_ENVIRONMENT"
+      value = var.contentful_environment
+      target = ["production", "preview"]
+    },
+    {
+      key = "VITE_CONTENTFUL_MANAGEMENT_TOKEN"
+      value = var.contentful_management_token
       target = ["production", "preview"]
     }
   ]
