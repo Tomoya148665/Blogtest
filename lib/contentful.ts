2d3e35f3686c4270194d92c91d93e9428d1282e2
Tomoya148665 <kopeyama27@gmail.com>
2023-09-11 17:56:34 +0900

    s

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -2,7 +2,7 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_ACCESS_TOKEN,
   host: "preview.contentful.com",
 });
 
